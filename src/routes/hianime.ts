31a50427f7c44da6d1f6d82652d8588c84ba0dcd
cscheah5 <cheahchoonsam5@gmail.com>
2025-03-18 21:39:59 +0800

    ci: test ci

--- src/routes/hianime.ts
+++ src/routes/hianime.ts
@@ -10,8 +10,7 @@ const hianimeRouter = new Hono<{ Variables: AniwatchAPIVariables }>();
 hianimeRouter.get("/", (c) => c.redirect("/", 301));
 
 // /api/v2/hianime/home
-//hianimeRouter.get("/home", async (c) => {
-hianimeRouter.get("/", async (c) => {
+hianimeRouter.get("/home", async (c) => {
   const cacheConfig = c.get("CACHE_CONFIG");
 
   const data = await cache.getOrSet<HiAnime.ScrapedHomePage>(
